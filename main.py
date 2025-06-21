1a9a2e02b82bebadac6695436decd4c989ff735a
Flartiny <1335348298@qq.com>
2025-06-21 10:36:27 +0800

    init

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.core import AstrBotConfig
 from astrbot.api import logger
+from .message import DanmakuMessage
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+SCOPE = "user-modify-playback-state user-read-playback-state"
+@register("astrbot_plugin_blive_spotify", "Flartiny", "", "")
+class BliveSpotify(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.context = context
+        self.config = config
+        client_id = self.config.get("client_id","")
+        client_secret = self.config.get("client_secret","")
+        redirect_uri = self.config.get("redirect_uri","")
+
+        star = self.context.get_registered_star("astrbot_plugin_bilibili_live")
+        self.bilibili_live_plugin = star.star_cls
+        self.spotify_client = self._get_spotify_client(client_id, client_secret, redirect_uri)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        if self.bilibili_live_plugin:
+            self.bilibili_live_plugin.register_message_subscriber(self.spotify_jukebox)
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    def _get_spotify_client(self, client_id, client_secret, redirect_uri):
+        auth_manager = SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=SCOPE,
+            open_browser=False
+        )
+
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        return sp
+
+    async def spotify_jukebox(self, message):
+        if isinstance(message, DanmakuMessage):
+            content = message.content
+            # 形如"点歌 name"
+            if content.startswith("点歌"):
+                search_query = content[2:]
+                results = self.spotify_client.search(q=search_query, type='track', limit=1)
+                if results['tracks']['items']:
+                    track_item = results['tracks']['items'][0]
+                    track_uri = track_item['uri']
+
+                    try:
+                        self.spotify_client.add_to_queue(uri=track_uri)
+                    except spotipy.exceptions.SpotifyException as e:
+                        logger.error("请确认你的 Spotify 账户是 Premium (付费) 会员。")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        if self.bilibili_live_plugin:
+            self.bilibili_live_plugin.unregister_message_subscriber(self.spotify_jukebox)
